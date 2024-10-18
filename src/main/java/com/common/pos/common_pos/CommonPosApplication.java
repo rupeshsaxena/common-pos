3f2c14d3e0b19d51b06708a02de923d8cfe3043c
Rupesh Saxena <rupesh.saxena20@gmail.com>
2024-10-18 18:05:15 +0530

    Add config files

--- src/main/java/com/common/pos/common_pos/CommonPosApplication.java
+++ src/main/java/com/common/pos/common_pos/CommonPosApplication.java
@@ -2,7 +2,11 @@ package com.common.pos.common_pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@EnableConfigServer
+@Configuration
 @SpringBootApplication
 public class CommonPosApplication {
 
