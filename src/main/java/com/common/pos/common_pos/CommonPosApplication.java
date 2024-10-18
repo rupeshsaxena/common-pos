ce3c0f288969c590f8e664b3c22f486c80b3afd6
Rupesh Saxena <rupesh.saxena20@gmail.com>
2024-10-18 18:02:03 +0530

    Initial Commit

---
+++ src/main/java/com/common/pos/common_pos/CommonPosApplication.java
@@ -0,0 +1,13 @@
+package com.common.pos.common_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/common/pos/common_pos/CommonPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.pos.common_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
